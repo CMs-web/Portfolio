9e1d75c8235fb8523b482a6a71a3b8f9089964e8
Shivam Goswami <shivamgoswami.work@gmail.com>
2024-05-27 13:07:28 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
 
   return (
-    <nav className="hidden px-6 z-[100] py-10 bg-neutral-100 lg:w-[14%] border-r border-neutral-200/[0.8] fixed top-0 h-screen lg:relative  left-0 lg:flex flex-col justify-between max-w-[14rem]">
+    <nav className="px-6 z-[100] py-10 bg-neutral-100 lg:w-[14%] border-r border-neutral-200/[0.8] fixed top-0 h-screen lg:relative  left-0 lg:flex flex-col justify-between max-w-[14rem]">
       <div className="flex items-center">
         <img
           className="rounded-3xl w-10 object-cover object-right h-10"
@@ -217,8 +217,9 @@ const Navbar = () => {
         </a>
       </div>
       <a
+        target="_blank"
         className="bg-slate-900 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
-        href="/resume"
+        href="/Shivam Goswami.pdf"
       >
         <span className="absolute inset-0 overflow-hidden rounded-full ">
           <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
